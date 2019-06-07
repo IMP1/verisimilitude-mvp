31f925885521067ba427121343007b5a34cd755c
IMP1 <huw_taylor@hotmail.co.uk>
2019-06-07 16:22:16 +0100

    Create map.lua

---
+++ map.lua
@@ -0,0 +1,11 @@
+local map = {}
+
+-- TODO: model a house/tavern/whatever in blender with simple shapes
+--       maybe check out Kenney's asset forge?
+-- TODO: export model to sprites and use as backgrounds and objects.
+
+function map.draw()
+    -- TODO: draw sprites
+end
+
+return map
